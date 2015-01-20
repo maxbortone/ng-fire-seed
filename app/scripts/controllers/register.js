f79e2075a76c608b93aac68ea254f4d65b0e0292
Max Bortone <maxbortone@gmail.com>
2015-01-20 23:36:49 +0100

    added register page

---
+++ app/scripts/controllers/register.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngFireQbotApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the ngFireQbotApp
+ */
+angular.module('ngFireQbotApp')
+  .controller('RegisterCtrl', function ($scope) {
+    $scope.createAccount = function(email, pass, confirm, name) {
+      $scope.err = null;
+      if( !pass ) {
+        $scope.err = 'Please enter a password';
+      }
+      else if( pass !== confirm ) {
+        $scope.err = 'Passwords do not match';
+      }
+      else {
+        simpleLogin.createAccount(email, pass, name)
+        .then(redirect, showError);
+      }
+    };
+
+
+    function redirect() {
+      $state.go('account');
+    }
+
+    function showError(err) {
+      $scope.err = err;
+    }
+  });
